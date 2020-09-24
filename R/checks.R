a9276420b988586225dc712565aec182bd81b36a
Nick Williams <ntwilliams.personal@gmail.com>
2020-09-24 16:36:49 -0400

    removing sl3 check

--- R/checks.R
+++ R/checks.R
@@ -1,18 +1,5 @@
 
-check_name_conflicts <- function(data) {
-
-
-}
-
 check_na_coef <- function(coefs) {
   coefs[is.na(coefs)] <- 0
   return(coefs)
-}
-
-check_sl3 <- function() {
-  if (length(find.package("sl3", quiet = TRUE)) == 0) {
-    FALSE
-  } else {
-    TRUE
-  }
 }
