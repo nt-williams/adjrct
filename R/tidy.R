a0d1c0fb9c1aa595bac054450bcfa06f1dee65db
Nick Williams <ntwilliams.personal@gmail.com>
2021-04-29 13:21:10 -0400

    basic tidy methods.

--- R/tidy.R
+++ R/tidy.R
@@ -15,22 +15,58 @@ tidy.rmst <- function(x, ...) {
 }
 
 #' @export
-tidy.survprob <- function(x, ...) {
-  tidy.rmst(x)
-}
+tidy.survprob <- tidy.rmst
 
+#' @export
 tidy.lor <- function(x, ...) {
-
+  tmp <- x$estimates[-which(names(x$estimates) %in% c("eif", "lo1.ci", "lo0.ci", "ci"))]
+  cis <- lapply(x$estimates[c("lo1.ci", "lo0.ci", "ci")], function(x) {
+    names(x) <- c("conf.low", "conf.high")
+    as.list(x)
+  })
+  out <- as.data.frame(c(tmp, as.list(unlist(cis))))
+  class(out) <- c("tbl_df", "tbl", "data.frame")
+  return(out)
 }
 
+#' @export
 tidy.cdf <- function(x, ...) {
-
+  dist <- t(x$estimates$dist)
+  std.error <- x$estimates$std.error
+  rownames(std.error) <- c("theta1.std.error", "theta0.std.error")
+  ci <- x$estimates$ci
+  ci <- mapply(function(x, n) {
+    colnames(x) <- paste(n, c("conf.low", "conf.high"), sep = ".")
+    x
+  }, ci, names(ci), SIMPLIFY = FALSE)
+  out <- cbind(k = x$levels[-length(x$levels)], as.data.frame(dist),
+               as.data.frame(t(std.error)), as.data.frame(do.call("cbind", ci)))
+  class(out) <- c("tbl_df", "tbl", "data.frame")
+  return(out)
 }
 
+#' @export
 tidy.pmf <- function(x, ...) {
-
+  dist <- t(x$estimates$dist)
+  std.error <- x$estimates$std.error
+  rownames(std.error) <- c("theta1.std.error", "theta0.std.error")
+  ci <- x$estimates$ci
+  ci <- mapply(function(x, n) {
+    colnames(x) <- paste(n, c("conf.low", "conf.high"), sep = ".")
+    x
+  }, ci, names(ci), SIMPLIFY = FALSE)
+  out <- cbind(k = x$levels, as.data.frame(dist),
+               as.data.frame(t(std.error)), as.data.frame(do.call("cbind", ci)))
+  class(out) <- c("tbl_df", "tbl", "data.frame")
+  return(out)
 }
 
+#' @export
 tidy.mannwhit <- function(x, ...) {
-
+  out <- data.frame(theta = x$estimates$theta,
+                    std.error = x$estimates$std.error[1, 1],
+                    conf.low = x$estimates$ci[1],
+                    conf.high = x$estimates$ci[2])
+  class(out) <- c("tbl_df", "tbl", "data.frame")
+  return(out)
 }
